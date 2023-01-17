e63d151fc056cd68f5cc9bfd2b8b1ee7dadb0aaa
Muhammad Asim <muhammadasim4515@gmail.com>
2023-01-17 19:49:41 +0500

    changes homepage 17 jan 23 (07)

--- components/Header.js
+++ components/Header.js
@@ -16,11 +16,13 @@ const Header = ({ bgColor, position }) => {
   return (
     <>
       <div
-        className={`${position} h-auto sm:w-full flex justify-between px-5 py-4 items-center w-[100%] ${bgColor} `}
+        className={`${position} h-auto sm:w-full flex justify-between px-5 py-4 items-center w-[100%] ${bgColor} bg-primaryColor`}
       >
         <Link href="/">
           <>
-            <img src="assets/maidzly-logo.png" height={35} width={168} alt="" />
+            <img src="/assets/logo-3.png" alt="" width={195} />
+
+            {/* <img src="assets/maidzly-logo.png" height={35} width={168} alt="" /> */}
             {/* <Image
               src="assets/maidzly-logo.png"
               height={35}
