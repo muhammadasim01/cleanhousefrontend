acd8b7047b900e4b188a45d6a6dffe1539605c7d
Muhammad Asim <muhammadasim4515@gmail.com>
2023-01-18 15:52:52 +0500

    changes homepage 18 jan 23 (01)

--- components/Home/Banner.js
+++ components/Home/Banner.js
@@ -17,21 +17,23 @@ const Banner = () => {
     <>
       <div className="bg-[url('../public/assets/Mobile-Banner.png')] bg-top sm:bg-center bg-no-repeat h-[700px] w-[100%] bg-cover  sm:h-[780px]  md:h-[700px] md:bg-center lg:bg-cover xl:bg:contain 2xl:bg:contain xl-h-[500px] 2xl:h-[800px]">
         <div className="flex flex-row items-center justify-center pt-[9rem] md:pt-[15rem] lg:pt-[10rem] xl:pt-[7rem]">
-          <div className="flex flex-col">
-            <span className="flex self-start px-7 pb-5">
+          <div className="flex flex-col  w-[100%]">
+            <span className="flex self-start pl-7 pb-5 ">
               <img src="./assets/star.png" width={17} height={0} />
               <img src="./assets/star.png" width={17} height={0} />
               <img src="./assets/star.png" width={17} height={0} />
               <img src="./assets/star.png" width={17} height={0} />
               <img src="./assets/star.png" width={17} height={0} />
             </span>
-            <h1 className="font-heading text-lightColor text-[40px] leading-[2.7rem] font-bold px-7  w-[90%]">
-              Book a local cleaner in your area <span />
+            <h1 className="font-heading text-lightColor text-[40px] leading-[2.7rem] font-bold pl-7  w-[100%] ">
+              <p>Book a local</p>
+              <p className="">cleaner in your</p>
+              area <span />
               <span className="font-subHeading text-lg font-light">
                 Canada & USA
               </span>
             </h1>
-            <p className="text-lightColor font-text text-lg font-medium py-5 px-7">
+            <p className="text-lightColor font-text text-lg font-medium py-5 pl-7 ">
               <span className="flex items-center font-semibold mb-2">
                 <BsCheckLg className="mr-2 text-secondaryColor" />
                 Rated 5 star cleaners.
@@ -46,11 +48,11 @@ const Banner = () => {
                 cleaner is on route.
               </span>
             </p>
-            <form className="flex relative items-center justify-center w-full pl-7 sm:w-[600px]">
+            <form className="flex relative items-center justify-center w-full pl-7 sm:w-[600px] ">
               <input
                 type="number"
                 placeholder="Enter your postcode/zip code"
-                className="h-14 w-full my-4 rounded-md font-medium text-normal text-darkColor"
+                className="h-14 w-[350px] my-4 rounded-md font-medium text-normal text-darkColor"
                 value={inputVal}
                 onChange={(e) => setInputVal(e.target.value)}
               />
@@ -58,7 +60,7 @@ const Banner = () => {
                 onClick={submitHandler}
                 className="p-1 w-12 h-11 bg-primaryColor  
               flex  text-lightColor relative right-12
-              rounded-md justify-center items-center sm:w-[13rem] sm:right-[10rem] "
+              rounded-md justify-center items-center sm:w-[13rem] sm:right-[10rem]  "
               >
                 <img src="./assets/search.png" alt="" width={20} />
                 <span className="hidden sm:flex">Search Now</span>
--- components/Home/ServiceCard.js
+++ components/Home/ServiceCard.js
@@ -8,22 +8,16 @@ function ServiceCard({
 }) {
   return (
     <div
-      className={`${background} bg-right-top p-8   bg-no-repeat  rounded-md my-3 flex flex-col items-center 
-                justify-center md:w-[350px] mx-2 lg:w-[480px] lg:h-[340px] xl:w-[600px] `}
-      style={{
-        backgroundPositionY: "-0.9rem",
-        backgroundPositionX: "-2rempx",
-        backgroundSize: "106% 107%",
-        git,
-      }}
+      className={`${background} bg-right-top bg-cover  bg-no-repeat bg-opacity-20 px-7  py-7 rounded-md my-6 flex flex-col items-center 
+              justify-center md:w-[350px] mx-2 lg:w-[480px] lg:h-[340px] xl:w-[600px] `}
     >
       <div className=" h-[40px] w-[40px] bg-secondaryColor p-2   rounded-full flex items-center justify-center text-2xl text-lightShade font-semibold">
         {Count}
       </div>
-      <h3 className="text-[40px] font-bold text-center pt-5 font-heading leading-[44px]">
+      <h3 className="text-[34px] font-bold text-center pt-3 font-heading leading-[44px]">
         {Title}
       </h3>
-      <p className="md:h-[350px] text-center font-text pt-5 text-[22px] font-normal">
+      <p className="md:h-[350px] text-center font-text pt-5 text-[18px] font-normal">
         {firstBox ? (
           <>
             Just enter your postcode/zip code on our website or click on the{" "}
@@ -37,7 +31,7 @@ function ServiceCard({
         )}
       </p>
       {buttonText && (
-        <div className="mt-5">
+        <div className="my-3">
           <button className="text-center mt-2 font-text w-[157px] h-[38px] text-[16px] font-bold text-lightColor bg-secondaryColor py-1  rounded-lg">
             {buttonText}
           </button>
